ec5b0d51087cd410f28b0491dee611500943d0dd
GearFox <favoringbasil55@gmail.com>
2022-05-03 17:27:02 -0400

    Created intake extend command

--- 2022HyperionRewrite/src/main/java/frc/robot/commands/IntakeExtend.java
+++ 2022HyperionRewrite/src/main/java/frc/robot/commands/IntakeExtend.java
@@ -4,23 +4,23 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Indexer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
+public class IntakeExtend extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final Indexer m_indexer;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public IntakeExtend(Indexer indexer) {
+    m_indexer = indexer;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(indexer);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +29,11 @@ public class ExampleCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_indexer.extendIntake();
+    m_indexer.driveIntake(Indexer.intakeSpeed);
+    m_indexer.driveMagazine(Indexer.magazineSpeed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -38,6 +42,6 @@ public class ExampleCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
