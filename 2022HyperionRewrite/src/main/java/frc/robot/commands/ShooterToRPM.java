f5b435a1ee07a9d85fc0e38687056cdbf62bf63c
GearFox <favoringbasil55@gmail.com>
2022-05-04 09:29:13 -0400

    Added shooter command

---
+++ 2022HyperionRewrite/src/main/java/frc/robot/commands/ShooterToRPM.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterToRPM extends CommandBase{
+    private final Shooter m_shooter;
+    private final double rpm;
+
+    public ShooterToRPM(Shooter shooter, Double rpm){
+        m_shooter = shooter;
+        this.rpm = rpm;
+    }
+
+    @Override
+    public void initialize(){}
+
+    @Override
+    public void execute(){
+        m_shooter.shootAtVelocity(rpm);
+    }
+    
+}
