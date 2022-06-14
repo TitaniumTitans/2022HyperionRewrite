7e9a37106f89f6b0b6511ab53a830018d2edb2d9
Maze <encryptedmaze@gmail.com>
2022-06-14 15:16:26 -0400

    Tried to reintroduce swerve code from main-old

--- src/main/java/org/titaniumtitans/frc2022/subsystems/Indexer.java
+++ src/main/java/org/titaniumtitans/frc2022/subsystems/Indexer.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import org.titaniumtitans.frc2022.Constants.IndexerConstants;;
+import org.titaniumtitans.frc2022.Constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
 
